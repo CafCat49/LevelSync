766be4551995804e7fe8a970003af6ca63d8bdd0
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-02-18 14:45:12 +0100

    Kotlin setup

---
+++ server/src/main/kotlin/com/cafcat/levelsync/LevelsyncApplication.kt
@@ -0,0 +1,11 @@
+package com.cafcat.levelsync
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LevelsyncApplication
+
+fun main(args: Array<String>) {
+	runApplication<LevelsyncApplication>(*args)
+}
