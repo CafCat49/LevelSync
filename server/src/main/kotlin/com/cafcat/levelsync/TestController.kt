42ab739ce1f9fb72e1fc38fe70c2a8b2105b5c7b
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-02-18 14:57:19 +0100

    Create TestController.kt

---
+++ server/src/main/kotlin/com/cafcat/levelsync/TestController.kt
@@ -0,0 +1,13 @@
+package com.cafcat.levelsync
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    
+    @GetMapping("/api/test")
+    fun test(): String {
+        return "Backend is working!"
+    }
+}
