34bb951d51ceb2dd953a4cac85e4b3e79d20f935
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-03-24 14:35:22 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
-  }
+  },
+  server: {
+    proxy: {
+      // Any request starting with '/api' will be redirected
+      '/api': {
+        target: 'https://levelsync-backend.onrender.com',
+        changeOrigin: true,
+        secure: true, // Set to false if you encounter SSL issues on Render
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
 })
